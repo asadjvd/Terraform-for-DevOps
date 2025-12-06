19160100070fc35989513a691530f7f17a840c6e
Asad Javed <asad.jvd00@gmail.com>
2025-12-06 12:18:42 +0500

    Added TF files

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.91.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "tf-state-bucket-practice-asad"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "tf-state-table"
+  }
+}
+
